c48653eca09c9553ecdb265cef4883802f48eaf1
RadicalRat <enderman9523@gmail.com>
2025-06-16 17:12:55 -0600

    switched to compressed images

--- src/rover_groundstation/rover_groundstation/image_sub.py
+++ src/rover_groundstation/rover_groundstation/image_sub.py
@@ -1,21 +1,21 @@
 import rclpy
 from rclpy.qos import qos_profile_sensor_data
 from rclpy.node import Node
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge
 import cv2
 
 class ImageSub(Node):
     def __init__(self):
         super().__init__("image_sub")
-        self.subscription = self.create_subscription(Image, '/camera/image_raw',self.image_callback,qos_profile=qos_profile_sensor_data)
+        self.subscription = self.create_subscription(CompressedImage, '/camera/image_raw/compressed',self.image_callback,qos_profile=qos_profile_sensor_data)
         self.cv = CvBridge()
 
     def image_callback(self, data):
         self.get_logger().info('recieved frame')
-        #current_frame = self.cv.imgmsg_to_cv2(data)
-        #cv2.imshow("Camera", current_frame)
-        #cv2.waitKey(1)
+        current_frame = self.cv.compressed_imgmsg_to_cv2(data)
+        cv2.imshow("Camera", current_frame)
+        cv2.waitKey(1)
     def destroy_node(self):
         return super().destroy_node()
     
