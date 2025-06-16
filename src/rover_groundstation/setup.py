30e79efb02e5873aa6a505fb2c725abca8522b47
RadicalRat <enderman9523@gmail.com>
2025-06-16 10:35:40 -0600

    fixed setup.py again

--- src/rover_groundstation/setup.py
+++ src/rover_groundstation/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'cameraviewer = rover_groundstation.image_sub:main.py',
+            'cameraviewer = rover_groundstation.image_sub:main',
         ],
     },
 )
