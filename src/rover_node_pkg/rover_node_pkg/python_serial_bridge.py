0ed47fae1adf9d8035e87d01e646b81adf4ae12a
RadicalRat <enderman9523@gmail.com>
2025-06-18 11:34:59 -0600

    fixed serial?

--- src/rover_node_pkg/rover_node_pkg/python_serial_bridge.py
+++ src/rover_node_pkg/rover_node_pkg/python_serial_bridge.py
@@ -17,7 +17,7 @@ class Serial(Node):
         self.declare_parameters(
             namespace='',
             parameters=[
-                ('COMPORT', "/dev/ttyAMA0"),
+                ('COMPORT', "/dev/ttyAMA10"),
                 ('BAUD', 115200),
                 ('PREFIX', "RoverTelem/roboclaw/"),
             ]
