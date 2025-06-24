bc2e896d45669dd27a053970b2e558a84d8e6088
RadicalRat <enderman9523@gmail.com>
2025-06-24 12:25:18 -0600

    raw image sub

--- src/rover_groundstation/rover_groundstation/image_sub.py
+++ src/rover_groundstation/rover_groundstation/image_sub.py
@@ -8,12 +8,12 @@ import cv2
 class ImageSub(Node):
     def __init__(self):
         super().__init__("image_sub")
-        self.subscription = self.create_subscription(CompressedImage, '/camera/image_raw/compressed',self.image_callback,qos_profile=qos_profile_sensor_data)
+        self.subscription = self.create_subscription(CompressedImage, '/camera/image_raw',self.image_callback,qos_profile=qos_profile_sensor_data)
         self.cv = CvBridge()
 
     def image_callback(self, data):
         self.get_logger().info('recieved frame')
-        current_frame = self.cv.compressed_imgmsg_to_cv2(data)
+        current_frame = self.cv.imgmsg_to_cv2(data)
         cv2.imshow("Camera", current_frame)
         cv2.waitKey(1)
     def destroy_node(self):
