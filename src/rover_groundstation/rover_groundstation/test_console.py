5c6d4b55138e5d0039a761c66b483b38457b9af4
RadicalRat <enderman9523@gmail.com>
2025-06-25 22:32:51 -0600

    test console complete?

--- src/rover_groundstation/rover_groundstation/test_console.py
+++ src/rover_groundstation/rover_groundstation/test_console.py
@@ -2,8 +2,11 @@ import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
 from rover_interfaces.action import TestCommand
-
+from rover_interfaces.srv import BagStart
+from std_srvs.srv import Trigger
 import rosbag2_py
+from pathlib import Path
+import time
 
 
 
@@ -13,34 +16,86 @@ class TestConsole(Node):
         self.action_cli = ActionClient(
             self,
             TestCommand,
-            'TestingActionServer',
-            self.action_callback
+            'vel_server',
         )
 
         #set up service client for lidar
 
         #set up service client for bag nodes
-        
-        
+        self.start_bag_serv = []
+        self.stop_bag_serv = []
+        self.discover_baggers()
+
+
+        bag_name = str(input("Bag folder name: "))
+        self.path = Path.home() + "/rosbags/" + bag_name
         lin_vel = float(input("Trial linear speed (cm/s): "))
         turn_rad = float(input("Trial turning radius (cm): (enter anything bigger than 1E6 for straight)"))
-        input("Press enter to start trial")
+        input("Press enter to start trial...")
 
         #start bag
+        self.start_bags()
 
-        #send lidar service command
+        #send lidar service command [todo]
+
 
         #wait 10s
+        time.sleep(10)
 
         #send rover action command
+        goal = TestCommand.Goal()
+        goal.linear_speed = lin_vel
+        goal.turning_radius = turn_rad
+        goal.run_duration = 40000
+        goal.accel_deaccel_duration = 5000
+        future = self.action_cli.send_goal_async(goal)
 
         #rover stops
-
-        #send lidar service command
+        rclpy.spin_until_future_complete(self, future=future)
+        
+        #send lidar service command [todo]
 
         #wait 10s
-        
+        time.sleep(10)
+
         #stop bag
+        self.stop_bags()
+        self.get_logger().info("trial complete.")
+
+    def discover_baggers(self):
+        
+        all_nodes = self.get_node_names()
+
+        for name in all_nodes:
+            if "bag" in name:
+                start_bag = self.create_client(BagStart, f"{name}/start")
+                stop_bag = self.create_client(Trigger, f"{name}/stop")
+
+                if not start_bag.wait_for_service(timeout_sec=2.0):
+                    self.get_logger().error(f"{name} start service not connected")
+                    continue
+
+                if not stop_bag.wait_for_service(timeout_sec=2.0):
+                    self.get_logger().error(f"{name} stop service not connected")
+                    continue
+
+                self.start_bag_serv.append(start_bag)
+                self.stop_bag_serv.append(stop_bag)
+
+                self.get_logger().info(f"connected to bag services on {name}")
+    
+    def start_bags(self):
+        request = BagStart().Request()
+        request.uri = self.path
+        for client in self.start_bag_serv:
+            client.call_async(request)
+            self.get_logger().info(f"started bagger")
+        return
+    
+    def stop_bags(self):
+        for client in self.stop_bag_serv:
+            client.call_async(Trigger.Request())
+            self.get_logger().info(f"stopped bagger")
         
 
 
---
+++ src/rover_groundstation/rover_groundstation/universal_bag.py
@@ -0,0 +1,97 @@
+import rclpy
+from rclpy.node import Node
+from rclpy.serialization import serialize_message
+from rover_interfaces.srv import BagStart
+from std_srvs.srv import Trigger
+
+import rosbag2_py
+import importlib
+
+class IMUBagger(Node):
+    def __init__(self):
+        super().__init__("universal_bagger_node")
+
+        self.declare_parameters(
+            namespace='',
+            parameters=[
+                ('topic_name', 'RoverTelem/roboclaw/enc_telem'),
+                ("msg_type", 'rover_interfaces.msg.MotorData'),
+                ('serialization_format', 'cdr'),
+            ]
+        )
+        
+        self.topic_name = self.get_parameter('topic_name').get_parameter_value().string_value
+        self.msg_type_str = self.get_parameter('msg_type').get_parameter_value().string_value
+        self.ser_format = self.get_parameter('serialization_format').get_parameter_value().string_value
+
+
+        pkg, msg = self.msg_type_str.split(".msg.")
+        msg_module = importlib.import_module(f"{pkg}.msg")
+        self.msg_type = getattr(msg_module, msg)
+
+
+        self.writer = rosbag2_py.SequentialWriter()
+
+        self.topic_info = rosbag2_py.TopicMetadata(
+            id=0,
+            name=self.topic_name,
+            type=self.msg_type_str.replace(".", "/"),
+            serialization_format=self.ser_format)
+        
+
+        self.subscription = self.create_subscription(
+            self.msg_type,
+            self.topic_name,
+            self.topic_callback,
+            10)
+        
+
+        self.start_serv = self.create_service(BagStart, self.get_name() + "_start",self.start_bag_callback)
+        self.stop_serv = self.create_service(Trigger, self.get_name() + "_stop", self.stop_bag_callback)
+
+        self.lock = True
+
+
+    def start_bag_callback(self, request, response):
+        storage_options = rosbag2_py.StorageOptions(
+            uri=request.uri,
+            storage_id='mcap')
+        converter_options = rosbag2_py.ConverterOptions('', '')
+        self.writer.open(storage_options, converter_options)
+        self.writer.create_topic(self.topic_info)
+        self.lock = False
+        self.get_logger().info(f"{self.get_name()} started bagging")
+        return response
+
+    def stop_bag_callback(self, request, response):
+        self.writer.close()
+        response.success = True
+        self.lock = True
+        self.get_logger().info(f"{self.get_name()} stopped bagging")
+        return response
+    
+
+
+    def topic_callback(self, msg):
+        if not self.lock:
+            self.writer.write(
+                self.topic_name,
+                serialize_message(msg),
+                self.get_clock().now().nanoseconds)
+            #self.get_logger().info("bagged msg")
+
+    def destroy_node(self):
+        self.writer.close()
+        return super().destroy_node()
+            
+def main(args=None):
+    rclpy.init(args=args)
+    node = IMUBagger()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
