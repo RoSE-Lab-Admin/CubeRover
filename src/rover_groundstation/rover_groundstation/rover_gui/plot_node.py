dfba9fddc371305f9f26857f4785442a2f7c9582
RadicalRat <enderman9523@gmail.com>
2025-07-07 18:32:31 -0600

    added plot updating for relevant topics

--- src/rover_groundstation/rover_groundstation/rover_gui/plot_node.py
+++ src/rover_groundstation/rover_groundstation/rover_gui/plot_node.py
@@ -1,8 +1,72 @@
 import rclpy
 from rclpy import Node
 
+#interfaces
+from rover_interfaces.msg import MotorData
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import PoseStamped
+
+from collections import deque
+
 class MultiPlot(Node):
     def __init__(self):
         super().__init__("plot_updater")
 
-    
+        self.create_subscription(MotorData, "/RoverTelem/roboclaw/enc_telem", self.motor_callback, 10)
+        self.create_subscription(Imu, "/RoverTelem/bno055/imu", self.imu_callback, 10)
+        self.create_subscription(PoseStamped, "/CubeRover_V1/pose", self.mocap_callback, 10)
+
+        self.motor_data = {
+            'time': deque(maxlen=100),
+            'pos': [deque(maxlen=100) * 4], #enc1, enc2, enc3, enc4
+            'vel': [deque(maxlen=100) * 4], # vel1, vel2, vel3, vel4
+            'volt': [deque(maxlen=100) * 2], #roboclaw volt1, volt2
+            'amp': [deque(maxlen=100) * 4], #roboclaw m1,m2,m3,m4 current draw
+        }
+
+        self.imu_data = {
+            'time': deque(maxlen=100),
+            'lin_accel': [deque(maxlen=100) * 3], #x, y, z acceleration
+        }
+
+        self.mocap_data = {
+            'time': deque(maxlen=100),
+            'pose:': [deque(maxlen=100) * 3], #x, y, z pos
+        }
+        
+        self.start = self.get_clock().now().nanoseconds * 1e-9
+
+    def motor_callback(self, msg):
+        self.motor_data['time'].append(self.get_clock().now().nanoseconds * 1e-9 - self.start)
+
+        self.motor_data['pos'][0].append(msg.enc1)
+        self.motor_data['pos'][1].append(msg.enc2)
+        self.motor_data['pos'][2].append(msg.enc3)
+        self.motor_data['pos'][3].append(msg.enc4)
+
+        self.motor_data['vel'][0].append(msg.vel1)
+        self.motor_data['vel'][1].append(msg.vel2)
+        self.motor_data['vel'][2].append(msg.vel3)
+        self.motor_data['vel'][3].append(msg.vel4)
+
+        self.motor_data['amp'][0].append(msg.m1current)
+        self.motor_data['amp'][1].append(msg.m2current)
+        self.motor_data['amp'][2].append(msg.m3current)
+        self.motor_data['amp'][3].append(msg.m4current)
+
+        self.motor_data['volt'][0].append(14)
+        self.motor_data['volt'][1].append(14)
+
+    def imu_callback(self, msg):
+        self.imu_data['time'].append(self.get_clock().now().nanoseconds * 1e-9 - self.start)
+
+        self.imu_data['lin_accel'][0].append(msg.linear_acceleration.x) #change to linaccel x
+        self.imu_data['lin_accel'][1].append(msg.linear_acceleration.y) #change to linaccel y
+        self.imu_data['lin_accel'][2].append(msg.linear_acceleration.z) #change to linaccel z
+
+    def mocap_callback(self, msg):
+        self.mocap_data['time'].append(self.get_clock().now().nanoseconds * 1e-9 - self.start)
+
+        self.mocap_data['pose'][0].append(msg.pose.position.x) #change to linaccel x
+        self.mocap_data['pose'][1].append(msg.pose.position.y) #change to linaccel y
+        self.mocap_data['pose'][2].append(msg.pose.position.z) #change to linaccel z
