2d03f0f0ce110c634d2c2ee4734ff2e2c4e8cd5c
RadicalRat <enderman9523@gmail.com>
2025-07-23 16:16:53 -0600

    changed ros2control launch file

---
+++ src/roseybot_control/bringup/launch/ros2_control.launch.py
@@ -0,0 +1,71 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler
+from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    path_to_urdf = PathJoinSubstitution([
+            FindPackageShare('roseybot_control'),
+            'description',
+            'urdf',
+            'roseybot.urdf.xacro'
+    ])
+
+    robot_description_content = Command([
+        FindExecutable(name='xacro'),
+        ' ',
+        path_to_urdf
+    ])
+
+    robot_description = {'robot_description': robot_description_content}
+
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare("roseybot_control"),
+            "config",
+            "diffbot_controllers.yaml",
+        ]
+    )
+    
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_controllers],
+        output="both",
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+    )
+
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "diffbot_base_controller",
+            "--param-file",
+            robot_controllers,
+            "--controller-ros-args",
+            "-r /diffbot_base_controller/cmd_vel:=/cmd_vel",
+        ],
+    )
+
+    nodes = [
+        robot_state_pub_node,
+    ]
+
+    return LaunchDescription(nodes)
