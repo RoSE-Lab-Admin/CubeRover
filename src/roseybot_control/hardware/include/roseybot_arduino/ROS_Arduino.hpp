2c4b61fdca0c7a1328f1281ab775462006f70db5
RadicalRat <enderman9523@gmail.com>
2025-09-26 14:54:35 -0600

    changed how serial is decoded on rpi. should be more stable but is skipping calls (needs fixing)

--- src/roseybot_control/hardware/include/roseybot_arduino/ROS_Arduino.hpp
+++ src/roseybot_control/hardware/include/roseybot_arduino/ROS_Arduino.hpp
@@ -47,6 +47,7 @@ public:
       try {
         serial_conn_.Open(serial_device);
         serial_conn_.SetBaudRate(convert_baud_rate(baud_rate));
+        serial_conn_.FlushIOBuffers();
         return;
       } catch (const LibSerial::OpenFailed& e) {
         RCLCPP_ERROR(logger_, "error opening serial port: %s - %s", serial_device.c_str(), e.what());
@@ -79,15 +80,31 @@ public:
     serial_conn_.FlushIOBuffers(); // Just in case
     serial_conn_.Write(msg_to_send);
 
+
+
     std::string response = "";
-    try
-    {
+    try {
+      auto start = std::chrono::high_resolution_clock::now();
+      char c = '\0';
+      while (true) {
+        if (serial_conn_.IsDataAvailable()) {
+          serial_conn_.ReadByte(c);
+          response += c;
+          if (c== '\n') break;
+        }
+        if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count() >= timeout_ms_) {
+          std::cout << response << ": timed out" << std::endl;
+          throw LibSerial::ReadTimeout("read timeout");
+        }
+      }
+
       // Responses end with \r\n so we will read up to (and including) the \n.
-      serial_conn_.ReadLine(response, '\n', timeout_ms_);
+      // response = serial_conn_.ReadLine(timeout_ms_);
     }
-    catch (const LibSerial::ReadTimeout&)
+    catch (const LibSerial::ReadTimeout& e)
     {
-        std::cerr << "The ReadByte() call has timed out." << std::endl ;
+        std::cerr << "Read timeout: " << e.what() << std::endl;
+        response = "dnf";
     }
 
     if (print_output)
@@ -107,9 +124,9 @@ public:
 
   void read_telem_values(std::vector<int> &telem)
   {
-    std::stringstream ss(send_msg("t\r\n"));
+    std::stringstream ss(send_msg("t\r"));
     std::string token;
-    std::cout << ss.str();
+    //std::cerr << ss.str() << std::endl;
     ss >> token;
     if (token != "e") {
         std::cerr << "Expected 'e' token but got: '" << token << "'" << std::endl;
@@ -131,7 +148,7 @@ public:
   {
     std::stringstream ss;
     ss << "m " << left_motors << " " << right_motors << "\r";
-    send_msg(ss.str());
+    serial_conn_.Write(ss.str());
   }
 
 
