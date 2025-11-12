228b18eea6462f3530d1d116eba2832440fd79c5
RadicalRat <enderman9523@gmail.com>
2025-11-12 09:59:17 -0700

    hardware launch file to launch imu and control

--- src/roseybot_control/bringup/launch/hardware_startup.launch.py
+++ src/roseybot_control/bringup/launch/hardware_startup.launch.py
@@ -1,23 +1,28 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-
 import os
+
+from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
-def generate_launch_description():
 
-    #launch file for imu
+#https://docs.ros.org/en/foxy/Tutorials/Intermediate/Launch/Using-ROS2-Launch-For-Large-Projects.html#writing-launch-files
+
+def generate_launch_description():
+    #launch file path for imu
     imu_launch = os.path.join(get_package_share_directory('roseybot_control'),'bringup', 'launch', 'imu_boot.launch.py')
 
-    #launch file for control
+    #launch file path for control
     control_launch = os.path.join(get_package_share_directory('roseybot_control'),'bringup', 'launch', 'ros2_control.launch.py')
 
-    return LaunchDescription([
-        IncludeLaunchDescription(
+    imu = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(imu_launch)
-        ),
-        IncludeLaunchDescription(
+        )
+
+    control  = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(control_launch)
         )
+    return LaunchDescription([
+        imu,
+        control
     ])
