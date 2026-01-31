b797ed5d1ee26a8dec39dc7f74bc67307d93223c
Connor Groeneweg <cgroenew@mines.edu>
2026-01-30 17:43:26 -0700

    Update test_example.py to only have hardware test mark. Added pytest.ini to avoid warnings when running only the hardware tests.

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,14 +1,10 @@
 @pytest.mark.skip(reason="This is an example test")
 def test_hello():
-    print("Hello, World!")
+    print("Hello, World! I'm a software test!")
     assert True # Some assertion here
     
 @pytest.mark.skip(reason="This is an example hardware test")
 @pytest.mark.hardware # Will use this to mark hardware tests
 def test_example_hardware():
-    assert True # Some assertion here
-
-@pytest.mark.skip(reason="This is an example software test")
-@pytest.mark.software # Will use this to mark software tests
-def test_example_software():
+    print("Hello, World! I'm a hardware test!")
     assert True # Some assertion here
