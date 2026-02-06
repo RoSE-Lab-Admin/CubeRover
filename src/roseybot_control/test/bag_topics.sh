f0a7323fde80b2d58626e9781ba694baaeb72dea
Lexi-jh <lexi.j.hanlon@gmail.com>
2026-02-06 22:34:21 +0000

    fixed bash script to make it a pytest

--- src/roseybot_control/test/bag_topics.sh
+++ src/roseybot_control/test/bag_topics.sh
@@ -1,15 +1,16 @@
+set -euo pipefail
 
-BAG_DIR="ros_bags"
+BAG_DIR="${BAG_DIR:-ros_bags}"
+DURATION="${DURATION:-15}"
+
 BAG_NAME="$(date '+%Y-%m-%d_%H-%M-%S')"
 BAG_PATH="$BAG_DIR/$BAG_NAME"
 
-DURATION=15
-
-cd /mnt/d/
+source /opt/ros/jazzy/setup.bash
 
 mkdir -p "$BAG_PATH"
 
-echo "Starting ROS2 Bag Recording"
+echo "BAG_PATH=$BAG_PATH"
 
 ros2 bag record -a -o "$BAG_PATH" & BAG_PID=$!
 
---
+++ src/roseybot_control/test/test_bag.py
@@ -0,0 +1,40 @@
+import subprocess
+import os
+from pathlib import Path
+import pytest
+
+@pytest.mark.hardware
+def test_bag():
+    # set paths
+    bag_dir = Path("/mnt/d/ros-bags")
+    script_path = Path(__file__).resolve().parent/"bag_topics.sh"
+
+    # set bash script variables
+    env = os.environ.copy()
+    env["BAG_DIR"] = str(bag_dir)
+    env["DURATION"] = "10"
+
+    # run bash script
+    result = subprocess.run(
+        ["bash", str(script_path)],
+        check=True,
+        text=True,
+        capture_output=True,
+        env=env
+    )
+
+    bag_path = None
+
+    # grab output of bash script
+    for line in result.stdout.splitlines():
+        if line.startswith("BAG_PATH="):
+            bag_path = Path(line.split("=", 1)[1].strip())
+            break
+        
+    # check if the bag path exists
+    assert bag_path.exists() and bag_path.is_dir(), f"Bag Folder Missing: {bag_path}"
+
+    # checks if the metadata file exists
+    meta_file = bag_path / "metadata.yaml"
+    assert meta_file.exists(), f"metadata.yaml missing"
+
