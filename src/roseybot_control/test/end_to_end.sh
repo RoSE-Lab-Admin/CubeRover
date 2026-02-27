c3f89221b8dfedd306e0ad15efcab97647fc44f5
Connor Groeneweg <cgroenew@mines.edu>
2026-02-27 22:01:01 +0000

    Update end_to_end.sh to use ros2 topic publishing command. Delete python end_to_end script.

--- src/roseybot_control/test/end_to_end.sh
+++ src/roseybot_control/test/end_to_end.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
-timeout 30 ros2 topic pub /cmd_vel geometry_msgs/msg/TwistStamped \
-"{header: {frame_id: base_link}, twist: {linear: {x: 0.10}}}"
-    
+LINEAR_VEL_X=1.5
+PUB_PER_SEC=10
+ros2 topic pub /cmd_vel geometry_msgs/msg/TwistStamped "{header: {frame_id: 'base_link'}, twist: {linear: {x: $LINEAR_VEL_X}}}" --rate $PUB_PER_SEC
