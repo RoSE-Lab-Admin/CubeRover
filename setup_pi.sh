2fde67032eef6f968243f2f3f04f6d301b2104e3
Lexi Hanlon <lexi.j.hanlon@gmail.com>
2026-03-04 15:21:50 -0700

    added bash script for setup

---
+++ setup_pi.sh
@@ -0,0 +1,12 @@
+source /opt/ros/jazzy/setup.bash
+sudo apt install \
+    ros-jazzy-navigation2 \
+    ros-jazzy-nav2-bringup 
+
+sudo apt-get install python3-rosdep
+sudo rosdep init
+rosdep update
+
+cd ~/Cuberover
+
+rosdep install --from-paths src -y --ignore-src
