d4b8579d6d5c541b265c0a3718a51e56a259bda2
Connor Groeneweg <cgroenew@mines.edu>
2026-05-17 03:47:21 +0000

    Make `sync.sh` send the folder and its contents

--- Testing/scripts/sync.sh
+++ Testing/scripts/sync.sh
@@ -4,16 +4,18 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source "${SCRIPT_DIR}/config.sh"
 
-LOCAL_WS_DIR="${SCRIPT_DIR}/../"
+# Use realpath to resolve the "../" into the actual, clean folder path without a trailing slash
+LOCAL_WS_DIR="$(realpath "${SCRIPT_DIR}/../")"
 
 echo "========================================"
 echo " Syncing workspace to Raspberry Pi..."
 echo "========================================"
 
+# Note: Remove the trailing slash from ${LOCAL_WS_DIR}
 rsync -avz --delete \
+    --exclude '.*/' \
     --exclude 'build/' \
     --exclude 'install/' \
     --exclude 'log/' \
-    --exclude '.git/' \
     --exclude 'Testing/' \
-    "${LOCAL_WS_DIR}" "${PI_USERNAME}@${PI_HOST}:${REMOTE_WS_DIR}/"
+    "${LOCAL_WS_DIR}" "${PI_USERNAME}@${PI_HOST}:${REMOTE_WS_DIR}"
